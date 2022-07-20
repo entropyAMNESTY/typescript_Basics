3baad49697e7784a5f8485134d865aab8703ff11
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-07-20 10:35:33 +0200

    Introduction to Typescript Basics

---
+++ variables/variables.js
@@ -0,0 +1,34 @@
+// Numbers
+var n1 = 10;
+console.log(n1);
+// Strings
+var s1 = 'You are the creator of your destiny';
+var s2 = "All the power is within you, you can do anything and everything";
+console.log(s1);
+console.log(s2);
+//Boolean
+var b1 = true;
+var b2 = false;
+console.log(b1);
+console.log(b2);
+//Any
+var a1 = 'test';
+var a2 = 123;
+var a3 = {
+    productId: 1,
+    product: 'bread',
+    productStock: true
+};
+console.log(a1);
+console.log(a2);
+console.log(a3);
+// Array
+var array1 = [
+    'AngularJS',
+    'ReactJS',
+    'VueJS',
+];
+console.log(array1);
+// Pick the first element within the array
+console.log(array1[0]);
+console.log(array1.length);
---
+++ variables/variables.ts
@@ -0,0 +1,45 @@
+// Numbers
+var n1: number = 10;
+console.log(n1);
+
+// Strings
+var s1: string = 'You are the creator of your destiny';
+var s2: string = `All the power is within you, you can do anything and everything`;
+console.log(s1);
+console.log(s2);
+
+//Boolean
+var b1: boolean = true;
+var b2: boolean = false;
+console.log(b1);
+console.log(b2);
+
+//Any
+
+var a1: any = 'test';
+var a2: any = 123;
+var a3: any = {
+    productId: 1,
+    product: 'bread',
+    productStock: true,
+};
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+
+// Array
+
+var array1: Array<string> = [
+    'AngularJS',
+    'ReactJS',
+    'VueJS',
+];
+
+console.log(array1);
+
+// Pick the first element within the array
+console.log(array1[0]);
+
+//Length of the Array
+console.log(array1.length);
